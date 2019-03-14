5429e097c3c752f78c4e293a52bf5e42b6477744
undacat <shannon@ruder.net>
2019-03-13 17:46:31 -0700

    initial commits

---
+++ app/js/home.js
@@ -0,0 +1,1 @@
+// optional JS for home page
--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-// Dependencies
-// =============================================================
+// first set up the dependencies
 var express = require("express");
-var path = require("path");
+var bodyParser = require('body-parser')
+var path = require('path');
 
-
-
-
-// Sets up the Express App
-// =============================================================
+// Tell node we're creating an express server
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+// set port to 8080 or whatever heroku (deployment site) sets it to
+var PORT = process.env.PORT || 8080;
+
+// express middleware needed for serving static files. For more details
+// see here: http://expressjs.com/en/starter/static-files.html
+app.use(express.static(__dirname + '/app/public'));
+
+// set up BodyParser to make it possible to interpret data sent to our server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// bring in api routes - bring in api routes first because
+// that's where we're getting our data to display in html pages
+require("./app/routing/api_routes.js")(app);
+
+// bring in the html routes
+require("./app/routing/html_routes.js")(app);
+
+// set up the listener
+app.listen(PORT, function() {
+	console.log("app listening on port", PORT);
+});
