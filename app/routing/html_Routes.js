c3eec073097a611a273204c0e4d4527b8d44d1d3
undacat <shannon@ruder.net>
2019-03-19 15:32:36 -0700

    routing

--- app/routing/html_Routes.js
+++ app/routing/html_Routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
 	});
 
 	// every other url path will take us to the home.html page
-	app.use(function(req, res) {
+	app.get("/", function(req, res) {
 		res.sendFile(path.join(__dirname + "/../public/home.html"));
 	});
 }
