e3bcf83c0bda7a921fdd6220eaa541498a95c254
undacat <shannon@ruder.net>
2019-03-20 17:13:41 -0700

    data and survey question fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,39 +2,39 @@
 var friends = [{
 	"name":"Tamandua",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Southern-Tamandua.png",
-	"scores":[4, 3, 3, 3, 4, 3, 2, 1, 5, 3]
+	"scores":[4, 4, 3, 3, 4, 5, 2, 5, 4, 5]
 }, {
 	"name":"Bush Baby",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Bush-Baby-1.png",
-	"scores":[3, 2, 5, 2, 4, 2, 3, 4, 2, 4]	
+	"scores":[3, 2, 5, 2, 3, 2, 2, 5, 5, 3]	
 }, {
 	"name":"Hedgehog",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Hedgehog-1.png",
-	"scores":[3, 2, 5, 2, 4, 2, 3, 4, 3, 3]
+	"scores":[3, 3, 5, 2, 3, 2, 2, 2, 3, 3]
 }, {
 	"name":"Llama",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Llama-1.png",
-	"scores":[5, 5, 1, 3, 1, 5, 1, 5, 5, 3]
+	"scores":[5, 5, 1, 3, 1, 5, 2, 1, 3, 2]
 }, {
 	"name":"Bearded Dragon",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Bearded-Dragon-Lizard-1.png",
-	"scores":[5, 5, 1, 3, 1, 5, 1, 5, 5, 2]
+	"scores":[4, 2, 1, 3, 5, 2, 3, 1, 1, 2]
 }, {
 	"name":"Hyacinth Macaw",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Hyacinth-Macaw.png",
-	"scores":[2, 2, 4, 4, 3, 2, 3, 2, 2, 4]
+	"scores":[2, 2, 3, 5, 4, 3, 1, 1, 4, 5]
 }, {
 	"name":"Chinchilla",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Chinchilla.png",
-	"scores":[1, 1, 4, 1, 5, 1, 4, 2, 1, 1]
+	"scores":[1, 1, 4, 1, 5, 1, 2, 4, 3, 1]
 }, {
 	"name":"Hermit Crab",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Hermit-Crab.jpg",
-	"scores":[4, 4, 3, 4, 4, 4, 5, 5, 4, 5]
+	"scores":[1, 1, 5, 4, 3, 1, 5, 3, 1, 4]
 }, {
 	"name":"Turtle",
 	"photo":"http://www.wordsiseek.com/wp-content/uploads/2017/10/Turtle.png",
-	"scores":[1, 1, 1, 5, 4, 1, 1, 1, 1, 1]
+	"scores":[1, 1, 5, 5, 3, 1, 5, 1, 1, 3]
 }];
 
 module.exports = friends;
